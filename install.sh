0cf291a98e2e25a93791f751b97543c2203e2e5f
Leonardo Rodrigues Ferreira <leorodriguesf95@gmail.com>
2025-07-12 10:22:36 -0300

    Add OS specific dependencies installation

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@ set -e
 
 OS_TYPE=$(uname -s)
 
-echo -n "=> "
-
 case "$OS_TYPE" in
-Darwin)
-    echo "macOS detected"
-    ;;
+Darwin) OS=macOS ;;
 Linux)
     if [ ! -f /etc/os-release ]; then
         echo "Not able to detect Linux distro. Stopping"
@@ -19,18 +15,83 @@ Linux)
     . /etc/os-release
 
     case "$ID" in
-    ubuntu)
-        echo "Ubuntu detected"
-        ;;
+    ubuntu) OS=Ubuntu ;;
     *)
+        echo "Linux distro $NAME not supported. Stopping"
         exit 1
         ;;
     esac
     ;;
 *)
-    echo "OS not yet supported. Stopping"
+    echo "OS $OS_TYPE not yet supported. Stopping"
     exit 1
     ;;
 esac
 
-echo -e "\nBegin installation (or abort with ctrl+c)..."
+ascii_art='
+ _                             _       _                           ___ 
+| |                           | |     (_)                         / __)
+| | _____  ___   ____ ___   __| | ____ _  ____ _   _ _____  ___ _| |__ 
+| || ___ |/ _ \ / ___) _ \ / _  |/ ___) |/ _  | | | | ___ |/___|_   __)
+| || ____| |_| | |  | |_| ( (_| | |   | ( (_| | |_| | ____|___ | | |   
+ \_)_____)\___/|_|   \___/ \____|_|   |_|\___ |____/|_____|___/  |_|   
+                                        (_____|                        
+'
+
+echo -e "$ascii_art"
+echo "=> Installation starting (or abort with ctrl+c)..."
+
+echo -e "\n$OS detected"
+echo "Installing $OS dependencies"
+
+case "$OS" in
+macOS)
+    # Prevent idle sleep while this script runs
+    # Use sudo to install Homebrew with root privileges to prevent installation failures.
+    sudo caffeinate -i -w $$ &
+
+    if [ ! -f /opt/homebrew/bin/brew ]; then
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null
+    fi
+
+    if ! command -v brew >/dev/null; then
+        eval "$(/opt/homebrew/bin/brew shellenv)" >/dev/null
+    fi
+
+    if ! command -v python3 >/dev/null; then
+        brew install python3 >/dev/null
+    fi
+
+    if ! command -v ansible >/dev/null; then
+        brew install ansible >/dev/null
+    fi
+    ;;
+Ubuntu)
+
+    if [ "$(id -u)" -eq 0 ]; then
+        alias sudo=""
+    fi
+
+    sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target >/dev/null 2>&1
+
+    sudo apt-get update >/dev/null
+
+    export DEBIAN_FRONTEND=noninteractive
+
+    if ! command -v git >/dev/null; then
+        sudo apt-get install git -y >/dev/null
+    fi
+
+    if ! command -v python3 >/dev/null; then
+        sudo apt-get install python3 -y >/dev/null
+    fi
+
+    if ! command -v ansible >/dev/null; then
+        sudo apt install software-properties-common -y >/dev/null
+        sudo add-apt-repository --yes --update ppa:ansible/ansible >/dev/null
+        sudo apt install ansible -y >/dev/null
+    fi
+
+    sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target >/dev/null 2>&1
+    ;;
+esac
