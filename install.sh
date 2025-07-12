9d6340a9a0dc1eed718f2ff1953662632e7eb901
Leonardo Rodrigues Ferreira <leorodriguesf95@gmail.com>
2025-07-11 22:42:01 -0300

    Add OS detection to allow installation of platform specific dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Hello world"
+set -e
+
+OS_TYPE=$(uname -s)
+
+echo -n "=> "
+
+case "$OS_TYPE" in
+Darwin)
+    echo "macOS detected"
+    ;;
+Linux)
+    if [ ! -f /etc/os-release ]; then
+        echo "Not able to detect Linux distro. Stopping"
+        exit 1
+    fi
+
+    . /etc/os-release
+
+    case "$ID" in
+    ubuntu)
+        echo "Ubuntu detected"
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
+    ;;
+*)
+    echo "OS not yet supported. Stopping"
+    exit 1
+    ;;
+esac
+
+echo -e "\nBegin installation (or abort with ctrl+c)..."
