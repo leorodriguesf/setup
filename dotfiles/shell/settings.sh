cca4e4c806d18c28cbf298e5e170b8d3b1294b6b
Leonardo Rodrigues Ferreira <leonardo.ferreira@ab-inbev.com>
2025-07-16 19:46:41 -0300

    Change folder to more meaningful name

--- dotfiles/shell/settings.sh
+++ dotfiles/shell/settings.sh
@@ -4,6 +4,6 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_BIN_HOME="$HOME/.local/bin"
 
-export USER_BACKUP_HOME="$HOME/backup"
+export BACKUP_DIR="$HOME/backup"
 
 export EDITOR=vim
